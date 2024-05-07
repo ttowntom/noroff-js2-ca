da5f0fe3772c41010ad43a4bfab8cfeb3d271b37
ttowntom <git.running054@passinbox.com>
2024-05-07 14:17:01 +0200

    adds JSDocs

--- src/js/handlers/dateTime.mjs
+++ src/js/handlers/dateTime.mjs
@@ -1,10 +1,19 @@
+/**
+ * Returns the date and time in a human-readable format
+ * @param {Date} timestamp - The timestamp to convert to a human-readable format
+ * @returns {string} - The date and time in a human-readable format
+ * ``` js
+ * const date = new Date();
+ * dateTime(date);
+ * // Returns a string with the current date and time
+ * ```
+ */
+
 export function dateTime(timestamp) {
 	const date = new Date(timestamp);
 	return date.toLocaleDateString("en-US", {
 		month: "long",
 		day: "numeric",
 		year: "numeric",
-		// hour: "numeric",
-		// minute: "numeric",
 	});
 }
